7ba67c56488d3d861a42177a582cc8eba220d91f
dev5151 <iamdev5151@gmail.com>
2020-04-19 02:54:20 +0530

    Created Update and Delete Routes

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -1,10 +1,9 @@
 const express=require('express');
 var router=express.Router();
 
+var ObjectId = require('mongodb').ObjectID;
 
 var {Employee}=require('../models/employee');
-//var ObjectId = require('mongodb').ObjectID;
-var ObjectId = require('mongodb').ObjectID;
 
 router.get('/',(req,res)=>{
     Employee.find((err,docs)=>{
@@ -44,6 +43,41 @@ router.post('/',(req,res)=>{
             console.log("Error in Employee Save :"+ JSON.stringify(err,undefined,2));
         }
     })
+});
+
+router.put('/:id',(req,res)=>{
+    if(!ObjectId.isValid(req.params.id)){
+        return res.status(400).send('No record with given id : ${req.params.id}');
+    }
+
+    // It is not an object of Employee Model, it is just a normal model 
+    var emp={
+        name:req.body.name,
+        position:req.body.position,
+        office:req.body.office,
+        salary:req.body.salary
+    };
+
+    Employee.findByIdAndUpdate(req.params.id,{$set:emp},{new:true},(err,doc)=>{
+        if(!err){
+            res.send(doc)
+        }else{
+            console.log("Error in Employee Update : "+JSON.stringify(err,undefined,2));
+        }
+    });
+});
+
+router.delete('/:id',(req,res)=>{
+    if(!ObjectId.isValid(req.params.id)){
+        return res.status(400).send('No record with given id : ${req.params.id}');
+    }
+    Employee.findByIdAndDelete(req.params.id,(err,doc)=>{
+        if(!err){
+            res.send(doc)
+        }else{
+            console.log("Error in Employee Delete : "+JSON.stringify(err,undefined,2));
+        }
+    });
 });
 
 module.exports=router;
